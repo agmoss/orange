b8dec768a4a153e7e3b6070ff4d9cec4d3f23206
Andrew Moss <andrew@m0ss.dev>
2020-02-16 21:34:35 -0700

    a number in state

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,23 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Html exposing (Html, button, div, h1, img, text)
 import Html.Attributes exposing (src)
+import Html.Events exposing (onClick)
+import Random
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { randRgb : Int }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model 1, Cmd.none )
 
 
 
@@ -22,12 +25,23 @@ init =
 
 
 type Msg
-    = NoOp
+    = UpdateNumber
+    | CreateNewNumber Int
+
+
+oneTo255 : Random.Generator Int
+oneTo255 =
+    Random.int 1 255
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        UpdateNumber ->
+            ( model, Random.generate CreateNewNumber oneTo255 )
+
+        CreateNewNumber val ->
+            ( Model val, Cmd.none )
 
 
 
@@ -38,8 +52,19 @@ view : Model -> Html Msg
 view model =
     div []
         [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        , h1 [] [ text "Orange" ]
+        , h1 [] [ text (String.fromInt model.randRgb) ]
+        , button [ onClick UpdateNumber ] [ text "Change Number" ]
         ]
+
+
+
+---- SUBSCRIPTIONS ----
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 
@@ -50,7 +75,7 @@ main : Program () Model Msg
 main =
     Browser.element
         { view = view
-        , init = \_ -> init
+        , init = init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
